7f0953bd43eb030d68999e07940101122a2c367c
Colin Watts <colinjoelwatts@gmail.com>
2022-12-08 13:49:50 -0500

    Solution to #6

---
+++ Day6_Signals.py
@@ -0,0 +1,31 @@
+def AreBufferCharactersAllDifferent(buffer):
+    lookup = {}
+    for val in buffer:
+        if not val in lookup.keys():
+            lookup[val] = 1
+        else:
+            return False
+
+    return True
+
+data = open("Day6Data.txt", 'r').read()
+
+buffer = []
+maxBufferSize = 4
+
+charCount = 0
+
+for i in range(len(data)):
+    charCount += 1
+
+    print(data[i])
+
+    buffer.append(data[i])
+    if len(buffer) > maxBufferSize:
+        buffer.pop(0)
+    
+        if AreBufferCharactersAllDifferent(buffer):
+            break
+
+print(charCount)
+
